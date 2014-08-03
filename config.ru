f023f01510824ba114d27c51bfa338438cac5884
Erik Benoist <ebenoist@groupon.com>
2014-08-03 10:41:20 -0500

    [frontend] added map display

--- config.ru
+++ config.ru
@@ -1,5 +1,5 @@
 use Rack::Static,
-  :urls => ["/images", "/js", "/css"],
+  :urls => ["/images", "/js", "/css", "/map"],
   :root => "public"
 
 run lambda { |env|
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,15 @@
+// Polymaps
+var po = org.polymaps;
 
+var map = po.map();
+var container = document.getElementById("map").appendChild(po.svg("svg"));
+
+map.container(container);
+map.add(po.interact());
+map.center({ lat: 41.9229, lon: -87.7072 });
+map.zoom(15);
+
+// Add the custom tiles as a map layer
+var tilesURL = po.url("/maps/{Z}/{X}/{Y}.png");
+var tiles = po.image().url(tilesURL);
+map.add(tiles);
